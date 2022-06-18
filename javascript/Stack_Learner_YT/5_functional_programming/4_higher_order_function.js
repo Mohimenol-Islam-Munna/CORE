61f3ac857729a97a9493855dbc7bb20bcdf7ffc8
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-18 10:55:39 +0600

    higher order function

--- javascript/Stack_Learner_YT/5_functional_programming/4_higher_order_function.js
+++ javascript/Stack_Learner_YT/5_functional_programming/4_higher_order_function.js
@@ -1,3 +1,8 @@
+// ----------------- Higher order function -----------------
+// 1. This function can takes function as an arguments 
+// 2. This function can return another function 
+
+
 function student(msg) {
   function name(value) {
     return `Hi ${msg}! ${value}`;
