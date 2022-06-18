5d04e0c8f1a7f7ad89503e039be551b0af9a9832
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-18 12:49:01 +0600

    git hoisting

---
+++ javascript/Stack_Learner_YT/6_scope_and_closure/1_hoisting.js
@@ -0,0 +1,18 @@
+var x = 10;
+console.log(x);
+
+y = 100;
+console.log(y);
+var y;
+
+let a = 20;
+console.log(a);
+
+let b;
+b = 200;
+console.log(b);
+
+if (x > 5) {
+  var z = 5;
+}
+console.log(z);
