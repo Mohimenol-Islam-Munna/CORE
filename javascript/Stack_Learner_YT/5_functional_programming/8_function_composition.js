018105f4644d414c6b1316aee3b3d31d0a0038f8
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-18 10:46:41 +0600

    functin composition

---
+++ javascript/Stack_Learner_YT/5_functional_programming/8_function_composition.js
@@ -0,0 +1,17 @@
+// ------------------ Function Composition --------------
+// 1. When result of one function pass as the argument/input of another function then it's called function composition.
+
+function mainFunc(a) {
+  return a * 100;
+}
+
+function one(b) {
+  return b / 10;
+}
+
+function two(c) {
+  return c + 5;
+}
+
+let result = mainFunc(one(two(20)));
+console.log("result :", result);
