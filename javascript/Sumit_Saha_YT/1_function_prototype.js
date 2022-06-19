83abfbfb60cf773ddc681a9ed4a2cb2c972bbf43
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-19 15:04:13 +0600

    function prototype

--- javascript/Sumit_Saha_YT/1_function_prototype.js
+++ javascript/Sumit_Saha_YT/1_function_prototype.js
@@ -1,4 +1,35 @@
-const common = {
+// ---------------- Function Prototype -----------------------
+// This is property of any js function which can poin to any object.
+// every function in js is a constructor. It can be called with new keyword. Constructor function name start with Capital leter(It's convension)
+
+
+
+// const common = {
+//   eat() {
+//     return "Player is eating..";
+//   },
+//   training() {
+//     return "Players are well trained";
+//   },
+// };
+
+function Player(name, age) {
+  const player = Object.create(Player.prototype);
+  player.name = name;
+  player.age = age;
+
+  return player;
+}
+
+function PlayerWithNew(name, age) {
+  // const this = Object.create(Player.prototype);
+  this.name = name;
+  this.age = age;
+
+  // return this;
+}
+
+Player.prototype = {
   eat() {
     return "Player is eating..";
   },
@@ -7,18 +38,19 @@ const common = {
   },
 };
 
-function Player(name, age) {
-  const player = Object.create(common);
-  player.name = name;
-  player.age = age;
-
-  return player;
-}
+PlayerWithNew.prototype = {
+  eat() {
+    return "Player is eating..";
+  },
+  training() {
+    return "Players are well trained";
+  },
+};
 
 const player1 = Player("Munna", 25);
 
-console.log("player1 : ", player1);
+console.log("player1 : ", player1.eat());
 
-const player2 = new Player("ifti", 15);
+const player2 = new PlayerWithNew("ifti", 15);
 
 console.log("player2 : ", player2.eat());
