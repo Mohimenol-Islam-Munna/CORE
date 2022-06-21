6141e51bfda11d930c4c00f337d14c68abdd7fda
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-21 12:13:33 +0600

    function with optional and default parameter

--- typescript/Codevolution/function.js
+++ typescript/Codevolution/function.js
@@ -1,4 +1,7 @@
 // ----------------------- Function -------------------------
+// 1. Every paramerters in ts are required
+// 2. It's possible to pass optional and default parameters in ts function.
+// 3. Optional parameters always come after required parameters
 // typescript function parameter with it's type
 function func1(num1, num2) {
     return num1 + num2;
@@ -14,3 +17,14 @@ function func3(num1, num2) {
     return num1 + num2;
 }
 console.log(func3(20, "3"));
+// fucntion with optional parameter
+function optionalParameterFunction(num1, num2) {
+    return "".concat(num1, " is required parameter value and ").concat(num2, " is optional parameter value");
+}
+console.log(optionalParameterFunction(true, 10));
+// fucntion with optional parameter
+function defaultParameterFunction(num1, num2) {
+    if (num2 === void 0) { num2 = 100; }
+    return "".concat(num1, " is required parameter value and ").concat(num2, " is default parameter value");
+}
+console.log(defaultParameterFunction(true));
--- typescript/Codevolution/function.ts
+++ typescript/Codevolution/function.ts
@@ -1,4 +1,7 @@
 // ----------------------- Function -------------------------
+// 1. Every paramerters in ts are required
+// 2. It's possible to pass optional and default parameters in ts function.
+// 3. Optional parameters always come after required parameters
 
 // typescript function parameter with it's type
 function func1(num1: number, num2: number) {
@@ -20,3 +23,17 @@ function func3(num1: number, num2: string): string | number {
 }
 
 console.log(func3(20, "3"));
+
+// fucntion with optional parameter
+function optionalParameterFunction(num1: boolean, num2?: number) {
+  return `${num1} is required parameter value and ${num2} is optional parameter value`;
+}
+
+console.log(optionalParameterFunction(true, 10));
+
+// fucntion with optional parameter
+function defaultParameterFunction(num1: boolean, num2: number = 100) {
+  return `${num1} is required parameter value and ${num2} is default parameter value`;
+}
+
+console.log(defaultParameterFunction(true));
