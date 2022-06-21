1ab9001a407f683acaf32b435b7b0ffbdd0c19f1
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-21 11:30:22 +0600

    types

---
+++ typescript/Codevolution/variables.js
@@ -0,0 +1,70 @@
+"use strict";
+// --------------------- variables ------------------
+// 1. variables of typescript almost same to the variables  of javascript
+// 2. Type in ts variables is optional
+// 3. There are three basic types of ts variables :: string, number, boolean.
+// 4. null and undefined are sub types of ts variables types
+// 5. syntax of variables :: let/const/var variable_name : type = value;
+exports.__esModule = true;
+// ----------------------- basic ------------------------
+// three basic types
+var name = "Munna";
+var isBeginner = true;
+var id = 160133;
+console.log("Three basic types ::", name, " ", isBeginner, " ", id);
+// sub types
+// this can be used in few times
+var contact = null;
+var price = undefined;
+// most use case
+// null and undefined can be stored in three basic types
+var phone = null;
+var isGraduated = undefined;
+console.log("Sub types ::", contact, " ", price, " ", phone, " ", isGraduated);
+// --------------------- array ------------------------
+// number array
+var arr1 = [1, 2, 3];
+var arr2 = [1, 2, 3];
+// string array
+var arr3 = ["js", "ts", "react", "vue", "react native"];
+var arr4 = [
+    "js",
+    "ts",
+    "react",
+    "vue",
+    "react native",
+    "node js",
+    "express js",
+];
+var arr5 = [true, false];
+var arr6 = [false, true];
+console.log("Array Declaration ::", arr1, " ", arr2, " ", arr3, " ", arr4, " ", arr5, " ", arr6);
+// ----------------------- array with mixed type ::: tuple type ---------------------
+// In this type order of type must be maintain in the order of value
+var mixedArryOrTuple = ["Munna", 33, true];
+console.log("Array Tuple/mixed Type Declaration ::", mixedArryOrTuple);
+// --------------------------  enum type ----------------------------
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+var clr = Color.blue;
+console.log("Color Enum :", clr);
+// ------------------------------ any type ---------------------------
+var anyType = "Munna";
+console.log("Any type ::", anyType);
+anyType = false;
+console.log("Any type ::", anyType);
+anyType = 15;
+console.log("Any type ::", anyType);
+// ------------------------------ unknown type ---------------------------
+var unknownType = "Munna";
+console.log("Unknown type ::", unknownType);
+unknownType = true;
+console.log("Unknown type ::", unknownType);
+unknownType = 15;
+console.log("Unknown type ::", unknownType);
+// type assertion 
+// console.log((unknownType as string).toUpperCase())
---
+++ typescript/Codevolution/variables.ts
@@ -0,0 +1,99 @@
+// --------------------- variables ------------------
+// 1. variables of typescript almost same to the variables  of javascript
+// 2. Type in ts variables is optional
+// 3. There are three basic types of ts variables :: string, number, boolean.
+// 4. null and undefined are sub types of ts variables types
+// 5. syntax of variables :: let/const/var variable_name : type = value;
+
+export {};
+
+// ----------------------- basic ------------------------
+// three basic types
+let name: string = "Munna";
+let isBeginner: boolean = true;
+let id: number = 160133;
+
+console.log("Three basic types ::", name, " ", isBeginner, " ", id);
+
+// sub types
+// this can be used in few times
+let contact: null = null;
+let price: undefined = undefined;
+
+// most use case
+// null and undefined can be stored in three basic types
+let phone: number = null;
+let isGraduated: boolean = undefined;
+
+console.log("Sub types ::", contact, " ", price, " ", phone, " ", isGraduated);
+
+// --------------------- array ------------------------
+// number array
+let arr1: number[] = [1, 2, 3];
+let arr2: Array<number> = [1, 2, 3];
+
+// string array
+let arr3: string[] = ["js", "ts", "react", "vue", "react native"];
+let arr4: Array<string> = [
+  "js",
+  "ts",
+  "react",
+  "vue",
+  "react native",
+  "node js",
+  "express js",
+];
+
+let arr5: boolean[] = [true, false];
+let arr6: Array<boolean> = [false, true];
+
+console.log(
+  "Array Declaration ::",
+  arr1,
+  " ",
+  arr2,
+  " ",
+  arr3,
+  " ",
+  arr4,
+  " ",
+  arr5,
+  " ",
+  arr6
+);
+
+// ----------------------- array with mixed type ::: tuple type ---------------------
+// In this type order of type must be maintain in the order of value
+let mixedArryOrTuple: [string, number, boolean] = ["Munna", 33, true];
+
+console.log("Array Tuple/mixed Type Declaration ::", mixedArryOrTuple);
+
+// --------------------------  enum type ----------------------------
+enum Color {
+  red,
+  green,
+  blue,
+}
+
+let clr: Color = Color.blue;
+
+console.log("Color Enum :", clr);
+
+// ------------------------------ any type ---------------------------
+let anyType: any = "Munna";
+console.log("Any type ::", anyType);
+anyType = false;
+console.log("Any type ::", anyType);
+anyType = 15;
+console.log("Any type ::", anyType);
+
+// ------------------------------ unknown type ---------------------------
+let unknownType: unknown = "Munna";
+console.log("Unknown type ::", unknownType);
+unknownType = true;
+console.log("Unknown type ::", unknownType);
+unknownType = 15;
+console.log("Unknown type ::", unknownType);
+
+// type assertion 
+// console.log((unknownType as string).toUpperCase())
