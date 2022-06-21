3f6d57b3ecdef14e5ea3bcd033b678bb43ebf55c
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-21 13:04:06 +0600

    inheritance

--- typescript/Codevolution/class.js
+++ typescript/Codevolution/class.js
@@ -1,4 +1,19 @@
-// ------------------------- Class ---------------------- 
+// ------------------------- Class ----------------------
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Employee = /** @class */ (function () {
     function Employee(name) {
         this.name = name;
@@ -10,3 +25,20 @@ var Employee = /** @class */ (function () {
 }());
 var munna = new Employee("Md Mohimenol Islam Munna");
 console.log(munna.greetring());
+// inheritance
+var Developer = /** @class */ (function (_super) {
+    __extends(Developer, _super);
+    function Developer(developer, age) {
+        var _this = _super.call(this, developer) || this;
+        _this.age = age;
+        return _this;
+    }
+    Developer.prototype.workingStatus = function () {
+        return "".concat(this.age, " years old developer done great job!");
+    };
+    return Developer;
+}(Employee));
+var reactDev = new Developer("Md Munna", 26);
+console.log(reactDev.name);
+console.log(reactDev.greetring());
+console.log(reactDev.workingStatus());
--- typescript/Codevolution/class.ts
+++ typescript/Codevolution/class.ts
@@ -1,17 +1,37 @@
-// ------------------------- Class ---------------------- 
+// ------------------------- Class ----------------------
 
 class Employee {
-    name : string
+  name: string;
 
-    constructor(name : string){
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    greetring(){
-        return `Hello ${this.name}, Welcome To typescript world!`;
-    }
+  greetring() {
+    return `Hello ${this.name}, Welcome To typescript world!`;
+  }
 }
 
 let munna = new Employee("Md Mohimenol Islam Munna");
 
-console.log(munna.greetring())
+console.log(munna.greetring());
+
+// inheritance
+
+class Developer extends Employee {
+  age: number;
+  constructor(developer: string, age: number) {
+    super(developer);
+    this.age = age;
+  }
+
+  workingStatus() {
+    return `${this.age} years old developer done great job!`;
+  }
+}
+
+let reactDev = new Developer("Md Munna", 26);
+
+console.log(reactDev.name);
+console.log(reactDev.greetring());
+console.log(reactDev.workingStatus());
