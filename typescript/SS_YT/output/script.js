ff4df5aae74d5224b5208ca769a34170f299dad2
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-21 15:47:45 +0600

    object type

--- typescript/SS_YT/output/script.js
+++ typescript/SS_YT/output/script.js
@@ -1,2 +1,22 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Hello TypeScript World!");
+// varialbles
+let name;
+name = "munna";
+let subject = "CSE";
+console.log(name);
+console.log(subject);
+// object
+let obj = {
+    name: "Munna",
+    age: 20,
+    zila: "Mymensingh",
+};
+let obj2;
+obj2 = {
+    name: "Md",
+    age: 40,
+    isGraduated: true,
+};
+console.log(obj2);
--- typescript/SS_YT/src/script.ts
+++ typescript/SS_YT/src/script.ts
@@ -1,1 +1,33 @@
+export {};
+
 console.log("Hello TypeScript World!");
+
+// varialbles
+let name;
+name = "munna";
+
+let subject: string = "CSE";
+
+console.log(name);
+console.log(subject);
+
+// object
+let obj: object = {
+  name: "Munna",
+  age: 20,
+  zila: "Mymensingh",
+};
+
+let obj2: {
+  name: string;
+  age: number;
+  isGraduated: boolean;
+};
+
+obj2 = {
+  name: "Md",
+  age: 40,
+  isGraduated: true,
+};
+
+console.log(obj2);
