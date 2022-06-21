e0ec861b187fbe5ce4949ce8e2e66dcd7bc9b79b
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-21 11:58:06 +0600

    multiple function return type

--- typescript/Codevolution/function.js
+++ typescript/Codevolution/function.js
@@ -1,12 +1,16 @@
 // ----------------------- Function -------------------------
-console.log("Typescript Function :");
-// typescript function parameter with it's type  
+// typescript function parameter with it's type
 function func1(num1, num2) {
     return num1 + num2;
 }
 console.log(func1(2, 3));
-// type script function return type 
+// type script function return type
 function func2(num1, num2) {
     return num1 + num2;
 }
 console.log(func2(20, 3));
+// type script function return with multiple type
+function func3(num1, num2) {
+    return num1 + num2;
+}
+console.log(func3(20, "3"));
--- typescript/Codevolution/function.ts
+++ typescript/Codevolution/function.ts
@@ -1,18 +1,22 @@
 // ----------------------- Function -------------------------
 
-
-console.log("Typescript Function :");
-
-// typescript function parameter with it's type  
-function func1(num1: number, num2:number){
-    return num1+num2
+// typescript function parameter with it's type
+function func1(num1: number, num2: number) {
+  return num1 + num2;
 }
 
-console.log(func1(2,3))
+console.log(func1(2, 3));
 
-// type script function return type 
-function func2(num1: number, num2:number): number{
-    return num1+num2;
+// type script function return type
+function func2(num1: number, num2: number): number {
+  return num1 + num2;
 }
 
-console.log(func2(20,3))
+console.log(func2(20, 3));
+
+// type script function return with multiple type
+function func3(num1: number, num2: string): string | number {
+  return num1 + num2;
+}
+
+console.log(func3(20, "3"));
