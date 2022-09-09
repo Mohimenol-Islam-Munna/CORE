ae48f2ecbc826bbcd4cd7b11cce466552bb3210d
unknown <munna.cse.pust@gmail.com>
2022-09-09 16:46:00 +0600

    basics types

---
+++ typescript/Maxmillian/1-Basic-types.js
@@ -0,0 +1,64 @@
+console.log("TypeScript All Basic Types : here");
+// Basic Types ::
+// 1. string :
+var playerName = "messi";
+var playerClub;
+var playerCountry = "argentina";
+playerClub = "psg";
+console.log("playerClub :", playerClub);
+console.log("playerCountry :", playerCountry);
+// 2. number
+var playerJersy = 10;
+var playerTrophy;
+playerTrophy = 101;
+var playerBDor = 5;
+console.log("playerTrophy :", playerTrophy);
+console.log("playerBDor :", playerBDor);
+// 3. Boolean
+var hasWc = true;
+var hasBDor;
+hasBDor = false;
+console.log("hasWc :", hasWc);
+// 4. array
+var playes = ["messi", "dybala", "mashrafe"];
+var jersy = [10, 9, 2];
+var sticker = [true, true, false];
+console.log("playes :", playes);
+// array alternative syntax
+var players2 = ["messi2", "dybala2", "mashrafe2"];
+// 5. object
+var playerInfo = {
+    name: "mash",
+    type: "Bowler",
+    jersy: 10
+};
+var playerInfo2 = {
+    name: "sakib",
+    type: "all-rounder",
+    jersy: 75
+};
+console.log("playerInfo2 :", playerInfo2);
+var playerInfo3 = {
+    name: "tamim",
+    type: "batter",
+    jersy: 28
+};
+console.log("playerInfo3 :", playerInfo3);
+var playerInfo4 = {
+    name: "tamim",
+    type: "batter",
+    jersy: 28
+};
+console.log("playerInfo4 :", playerInfo4.name);
+var playerInfo5 = {
+    name: "mushfiq",
+    type: "wk-Batter",
+    jersy: 15
+};
+console.log("playerInfo5 :", playerInfo5.type);
+var playerInfo6 = {
+    name: "Taskin",
+    type: "Bowler",
+    jersy: 3
+};
+console.log("playerInfo6 :", playerInfo6);
