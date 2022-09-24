41e9bb83910f3ffc78ad2682c5ad4fe3dc6e0eef
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-09-24 14:28:28 +0600

    constructor

--- JAVASCRIPT/OOP/1_constructor/index.js
+++ JAVASCRIPT/OOP/1_constructor/index.js
@@ -1,1 +1,24 @@
-console.log("console log");
+// In javascript every function is a constructor function.
+// Convention is write constructor function with capitial letter.
+// Constructor function is used to create or initialize object from this function with new keyword.
+// In modern javascript constructor function is used in class to create object from this class.
+
+function Fun(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+const f1 = new Fun("Nagordola", 30);
+
+console.log("f1 ::", f1);
+
+class Fun2 {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const f2 = new Fun2("Chorki", 25);
+
+console.log("f2 ::", f2);
